642455536dbbe053d1003a0fd7151ef303534a70
Mark McKinstry <mmckinst@umich.edu>
2016-08-05 10:18:11 -0400

    make test work on puppet 3.x

--- lib/puppet/parser/functions/hash2yaml.rb
+++ lib/puppet/parser/functions/hash2yaml.rb
@@ -13,7 +13,16 @@ This converts a puppet hash to YAML string.
     end
 
     h = arguments[0]
-
-    return h.to_yaml
+    # https://github.com/hallettj/zaml/issues/3
+    # https://tickets.puppetlabs.com/browse/PUP-3120
+    # https://tickets.puppetlabs.com/browse/PUP-5630
+    #
+    # puppet 3.x uses ZAML which formats YAML output differently than puppet 4.x
+    # including not ending the file with a new line
+    if Puppet.version.to_f < 4.0
+      return h.to_yaml << "\n"
+    else
+      return h.to_yaml
+    end
   end
 end
