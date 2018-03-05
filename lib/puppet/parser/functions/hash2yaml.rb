df1c3bd42e4094aa023f22de03fb811f91e39bb9
Johan Fleury <jfleury@arcaik.net>
2018-03-05 15:05:31 -0500

    Add a header setting to hash2yaml

--- lib/puppet/parser/functions/hash2yaml.rb
+++ lib/puppet/parser/functions/hash2yaml.rb
@@ -5,14 +5,29 @@ This converts a puppet hash to YAML string.
   ) do |arguments|
     require 'yaml'
 
-    if arguments.size != 1
-      raise(Puppet::ParseError, 'hash2yaml(): requires one and only one argument')
+    if arguments.size < 1
+      raise(Puppet::ParseError, 'hash2yaml(): requires at least one argument')
+    end
+    if arguments.size >= 3
+      raise(Puppet::ParseError, 'hash2yaml(): too many arguments')
     end
     unless arguments[0].is_a?(Hash)
       raise(Puppet::ParseError, 'hash2yaml(): requires a hash as argument')
     end
 
     h = arguments[0]
+
+    settings = {
+      'header' => '',
+    }
+
+    if arguments[1]
+      unless arguments[1].is_a?(Hash)
+        raise(Puppet::ParseError, 'hash2yaml(): Requires a hash as second argument')
+      end
+      settings.merge!(arguments[1])
+    end
+
     # https://github.com/hallettj/zaml/issues/3
     # https://tickets.puppetlabs.com/browse/PUP-3120
     # https://tickets.puppetlabs.com/browse/PUP-5630
@@ -20,9 +35,15 @@ This converts a puppet hash to YAML string.
     # puppet 3.x uses ZAML which formats YAML output differently than puppet 4.x
     # including not ending the file with a new line
     if Puppet.version.to_f < 4.0
-      return h.to_yaml << "\n"
+      output = h.to_yaml << "\n"
     else
-      return h.to_yaml
+      output = h.to_yaml
+    end
+
+    if settings['header'].to_s.empty?
+      return output
+    else
+      return "#{settings['header']}\n#{output}"
     end
   end
 end
--- spec/functions/hash2yaml_spec.rb
+++ spec/functions/hash2yaml_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'hash2yaml' do
   it { is_expected.not_to eq(nil) }
-  it { is_expected.to run.with_params().and_raise_error(Puppet::ParseError, /requires one and only one argument/) }
-  it { is_expected.to run.with_params({}, {}, {}).and_raise_error(Puppet::ParseError, /requires one and only one argument/) }
+  it { is_expected.to run.with_params().and_raise_error(Puppet::ParseError, /requires at least one argument/) }
+  it { is_expected.to run.with_params({}, {}, {}).and_raise_error(Puppet::ParseError, /too many arguments/) }
   it { is_expected.to run.with_params('some string').and_raise_error(Puppet::ParseError, /requires a hash as argument/) }
 
   example_input = {
@@ -52,5 +52,41 @@ EOS
 
     it { is_expected.to run.with_params(example_input).and_return(output) }
 
+  end
+
+  context "custom settings" do
+    settings = {
+      'header' => '# THIS FILE IS CONTROLLED BY PUPPET',
+    }
+
+    if Puppet.version.to_f < 4.0
+      output=<<-EOS
+# THIS FILE IS CONTROLLED BY PUPPET
+--- 
+  domain: example.com
+  mysql: 
+    hosts: 
+      - "192.0.2.2"
+      - "192.0.2.4"
+    user: root
+    pass: setec-astronomy
+  awesome: true
+EOS
+    else
+      output=<<-EOS
+# THIS FILE IS CONTROLLED BY PUPPET
+---
+domain: example.com
+mysql:
+  hosts:
+  - 192.0.2.2
+  - 192.0.2.4
+  user: root
+  pass: setec-astronomy
+awesome: true
+EOS
+    end
+
+    it { is_expected.to run.with_params(example_input, settings).and_return(output) }
   end
 end
